9174b339d83f841fa40266ea460a23bf8aad0b5c
Danil <litan.yt.terr@gmail.com>
2023-09-27 11:06:05 +0700

    2.0.0

--- js/script.js
+++ js/script.js
@@ -1,19 +1,37 @@
-const anchors = document.querySelectorAll('a[href*="#"]');
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 3500,
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
-anchors.forEach(anchor => {
-	anchor.addEventListener('click', (e) => {
-        e.preventDefault();
-        
-		const blockId = anchor.getAttribute('href').replace('#', '');
+  const anchors = document.querySelectorAll('a[href*="#"]');
 
-        const element = document.getElementById(blockId);
-        const headerOffset = 80;
-        const elementPosition = element.getBoundingClientRect().top;
-        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-    
-        window.scrollTo({
-            top: offsetPosition,
-            behavior: "smooth"
-        });
-        })
-})
+  anchors.forEach(anchor => {
+    anchor.addEventListener('click', (e) => {
+          e.preventDefault();
+          
+      const blockId = anchor.getAttribute('href').replace('#', '');
+  
+          const element = document.getElementById(blockId);
+          const headerOffset = 80;
+          const elementPosition = element.getBoundingClientRect().top;
+          const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+      
+          window.scrollTo({
+              top: offsetPosition,
+              behavior: "smooth"
+          });
+          })
+  })
